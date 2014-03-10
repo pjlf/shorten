f857411bda974728c062c0ded2ea6785705cfdbb
pjlfpt <pjlfpt@gmail.com>
2014-03-10 09:25:07 +0000

    versão server com autenticaçao

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 
 var express = require('express');
 var cors = require('cors');
+var crypto = require('crypto');
+var assert = require('assert');
+
+var HMAC_ALG = 'sha1';
+// var HMAC_KEY = process.env.KEY;
+var HMAC_KEY = 'glorioso';
+
+//var MAX_SESSION_SECONDS = 60 * 60 * 2;
+var MAX_SESSION_SECONDS = 60;
+
+assert(HMAC_KEY, 'KEY is not defined!');
 
 var app = express(), 
 	server = require('http').createServer(app), 
@@ -55,7 +66,49 @@ app.get('/:url', function(req, res){
 	res.send('url: ' + link);
 });
 
+// ----- Tratamento validação
+app.post('/login', function(req, res){
+	var user = req.body['user'];
+	var pass = req.body['password'];
+	var resp = {token: '', erro: ''}
+
+	console.log('user: ' + user + ', pass: ' + pass);
+
+	if(! validate(user, pass)){
+		resp.erro = 'Password inválida';
+		res.send(resp)
+	}
+
+	resp.token = generateToken(user);
+	res.send(resp);
+});
+
+function generateToken(username) {
+	var now = Math.floor(Date.now() / 1000);
+
+	var sign = crypto.createHmac(HMAC_ALG, HMAC_KEY).update(username+':'+now).digest('base64');
+
+	return JSON.stringify({ ts: now, username: username, sign: sign });
+}
+
+function validate(username, password) {
+	return username.substring(0, 3) === password;
+}
+// ----- Fim tratamento validação
+
 app.post('/', function(req, res){
+	var resposta = {obj: {}, erro: ''}
+
+	// só permite inserção de shortens se tiver um token válido
+	var token = req.header('X-Auth-Token');
+
+	if(! validateToken(token)){
+		console.log('Token inválido');
+		resposta.erro = 'Invalid token, not authorized';
+		res.send(resposta);
+		return;
+	}
+
 	if(req.body.hasOwnProperty('url')){
 
 		var shorten = undefined;
@@ -79,16 +132,36 @@ app.post('/', function(req, res){
 		}
 
 		console.log('ok');
-		res.send(req.body);
+		resposta.obj = req.body;
+		res.send(resposta);
 	}
 	else{
 		console.log('invalid');
-		res.send('Invalid shorten');
+		esposta.erro = 'Invalid shorten';
+		res.send(resposta);
 	}
 });
 
-io.sockets.on('connection', function (socket) {
+function validateToken(token){
+	var obj;
+	try {
+		obj = JSON.parse(token);
+	} catch(ex) {
+		return false;
+	}
 
+	if (Math.floor(Date.now()/1000) - obj.ts > MAX_SESSION_SECONDS)
+		return false;
+
+	return obj.sign === generateSign(obj.username, obj.ts);
+};
+
+function generateSign(username, ts) {
+	var sign = crypto.createHmac(HMAC_ALG, HMAC_KEY).update(username+':'+ts).digest('base64');
+	return sign;
+}
+
+io.sockets.on('connection', function (socket) {
 
 	function emitShorten(pShort){ socket.emit('respNewShorten', pShort); }
 
